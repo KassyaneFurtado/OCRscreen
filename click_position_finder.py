1bce10096c3b2b828c2c1d74b702be8705abd699
Kassyane Amanda Rodrigues Furtado <kassyanefurtado@outlook.com>
2024-02-16 12:23:57 -0300

    Corrigindo erro no DataFrame

---
+++ click_position_finder.py
@@ -0,0 +1,20 @@
+import time
+import pyautogui
+
+time.sleep(5)
+# Posição do click na tela
+print(pyautogui.position())
+
+# Teste de rolagem da tela
+# pyautogui.scroll(-1000)
+
+# Teste de coordenadas de Screenshot
+# Utilizar o pyautogui.position()) para obeter a coordenada dos clicks de canto superior esquerdo e canto inferior direito
+# Coordenadas do canto superior esquerdo 
+# print(pyautogui.position())
+# time.sleep(5)
+# Coordenadas do canto inferior direito
+# print(pyautogui.position())
+
+
+
--- screen_text_extractor_ocr.py
+++ screen_text_extractor_ocr.py
@@ -1,38 +1,31 @@
-import pyautogui
+# Antes de executar o código abrir o software e deixar na pagina de pesquisa
 import pytesseract
-import time
 from dotenv import load_dotenv
-import os
 import pandas
+import pyautogui
+import time
 import pandas as pd
-from PIL import Image
 
-
-# Abrir o prontuário eletrônico
-pyautogui.PAUSE=1
-pyautogui.click(x=1365, y=757)
-pyautogui.doubleClick(x=1313, y=530) #click duplo para abrir o software
-time.sleep(10)
-pyautogui.click(x=607, y=372)
 load_dotenv()
-pyautogui.write(os.getenv("usuariomvpep"))
-pyautogui.press("tab")
-pyautogui.write(os.getenv("senhamvpep"))
-pyautogui.press("enter")
-# Procurar por cada prontuário usando a base de dados
-time.sleep(20)
-pyautogui.click(x=563, y=716)
-time.sleep(2)
-pyautogui.click(x=342, y=191)
-pyautogui.click(x=406, y=315)
-pyautogui.click(x=120, y=252)
-pyautogui.click(x=172, y=353)
-pyautogui.click(x=178, y=192)
-tabela = pandas.read_csv("PCU.csv")
-for linha in tabela.index:
-    prontuario = tabela.loc[linha,"PRONTUARIO"]
-    pyautogui.write(str(int(tabela.loc[linha,"PRONTUARIO"])))
+time.sleep(3)
+pyautogui.PAUSE=1
+pytesseract.pytesseract.tesseract_cmd = "C:/tesseract/tesseract.exe"
+tabela = pandas.read_csv("PACIENTES.csv", converters={"DATANASCIMENTO": str, "PRONTUARIO": str})
+time.sleep(5) # Tempo para trocar para a tela do software
+# Adicionar filtros de pesquisa
+pyautogui.click(x=344, y=190)
+pyautogui.click(x=417, y=314)
+pyautogui.click(x=119, y=252)
+time.sleep(1)
+# Pesquisar prontuarios
+pyautogui.click(x=183, y=347)
+time.sleep(1)
+for i in tabela.index:
+    pyautogui.click(x=127, y=188)
+    prontuario = tabela.loc[i,"PRONTUARIO"]
+    pyautogui.write(prontuario)
     pyautogui.press("enter")
+    time.sleep(3)
     # Coordenadas do canto superior esquerdo
     x1, y1 = 902, 242
     # Coordenadas do canto inferior direito
@@ -42,10 +35,9 @@ for linha in tabela.index:
     height = y2 - y1
     # Captura a screenshot da área específica
     screenshot = pyautogui.screenshot(region=(x1, y1, width, height))
-    # Salve ou processe a screenshot conforme necessário
-    screenshot.save("data_nascimento.png")
-    pytesseract.pytesseract.tesseract_cmd = "C:/tesseract/tesseract.exe"
-    text = pytesseract.image_to_string("data_nascimento.png")
-    print(text)
-    df = pd.DataFrame(data=text,columns="DATANASCIMENTO")
-    df.head()
+    tabela.loc[i, "DATANASCIMENTO"] = pytesseract.image_to_string(screenshot).strip()
+    pyautogui.doubleClick(x=127, y=188)
+    pyautogui.press("delete")
+    tabela.to_csv("PACIENTES.csv", index=False)
+
+
